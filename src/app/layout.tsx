c801a0dcc5bcb4c66f755d789679cc45af6a4bd1
mymac <mymac@mymacui-Macmini.local>
2026-03-10 11:45:21 +0900

    3/10sdmcl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import "./globals.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -9,10 +9,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           name="description"
           content="진심이 만든 전략, 오래 찾는 마케팅. 픽스애드는 SEO, 퍼포먼스 마케팅, 콘텐츠 전략으로 비즈니스의 지속 가능한 성장을 만듭니다."
         />
+        <meta
+          name="naver-site-verification"
+          content="632ee8d841c3e6fc51d7a0324d9671535e7f2ef3"
+        />
       </head>
       <body>
         <div className="root">{children}</div>
       </body>
     </html>
-  )
+  );
 }
